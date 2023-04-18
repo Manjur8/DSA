9bce7ecac0a9cff63db4c02131ad385d504bba4b
Manjur <manjursk209@gmail.com>
2023-04-18 22:32:17 +0530

    added bst insert node & preOrder traversal

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 import { hashTable } from './HashTable/index.js'
 import { displayArr } from './DS/Array/index.js';
+import { treeOutput } from './DS/BST/index.js';
 
 const arrayBtn = document.getElementById('array');
 arrayBtn.addEventListener('click', () => {
@@ -10,5 +11,11 @@ const hashTableBtn = document.getElementById('hashTableBtn');
 hashTableBtn.addEventListener('click', () => {
     hashTable()
 })
+const bstBtn = document.getElementById('bstBtn');
+bstBtn.addEventListener('click', () => {
+    treeOutput()
+})
+
+
 
 document.getElementById('result').innerHTML = `<h1 class='text-center'>DSA<h2>`
